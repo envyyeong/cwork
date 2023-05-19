43bb0ad0ceb8a20aa5e7adb65aee7d7ee24b70ff
qaz68 <qaz68@MINYEONGSURFACE>
2023-05-19 09:04:18 +0900

    푸쉬 테스트

---
+++ Cwork/20230518exam/Testexam.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int StringCount(char* _Arr)
+{
+    int Ccount = 0;
+
+    while (true)
+    {
+        char i = *(_Arr + Ccount);
+
+        if ('0' == i)
+        {
+            break;
+        }
+        ++Ccount;
+    }
+    return Ccount;
+}
+
+int main()
+{
+    {
+        char Arr[100] = "aaijo";
+
+        int Result = StringCount(Arr);
+        cout << Result;
+        int a = 0;
+    }
+}
